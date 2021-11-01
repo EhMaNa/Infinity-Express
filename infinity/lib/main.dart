70252da60c6be2f8e53d686e6b0949f5156251ac
Darius <68332672+Darius253@users.noreply.github.com>
2021-11-01 01:31:02 +0000

    Changed from SignIn to SignUP

--- infinity/lib/main.dart
+++ infinity/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:infinity/pages/sign-in.dart';
+import 'package:infinity/pages/signup.dart';
 
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: SignIn());
+        home: SignUp());
   }
 }
 
