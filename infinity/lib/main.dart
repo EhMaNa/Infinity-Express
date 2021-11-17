022fe4ab7ef0973f94daec8ff2e90e3e965dda25
Darius <68332672+Darius253@users.noreply.github.com>
2021-11-17 00:10:12 +0000

    Refactor of code in main.dart

--- infinity/lib/main.dart
+++ infinity/lib/main.dart
@@ -4,11 +4,12 @@ import 'package:infinity/pages/sign-up.dart';
 
 
 void main() {
+  
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of our application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
