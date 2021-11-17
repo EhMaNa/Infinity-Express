6661aa6896b95cbd1cf051119e4987e029ee59b5
Darius <68332672+Darius253@users.noreply.github.com>
2021-11-17 00:11:39 +0000

    Refactor of code in styles.dart

--- infinity/lib/styles/styles.dart
+++ infinity/lib/styles/styles.dart
@@ -7,8 +7,8 @@ class BlueText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap(),
+    return TextButton(
+      onPressed: onTap(),
       child: Text(
         textName,
         style: TextStyle(
@@ -26,7 +26,6 @@ class Button extends StatefulWidget {
   final Function onTap;
   Button({required this.textName, required this.onTap});
 
-
   @override
   _ButtonState createState() => _ButtonState();
 }
@@ -41,18 +40,18 @@ class _ButtonState extends State<Button> {
         borderRadius: BorderRadius.circular(15),
         onTap: widget.onTap(),
         child: Container(
-          height: 83,
+          height: 60,
           decoration: BoxDecoration(
               border: Border.all(color: Colors.white),
-              borderRadius:
-              BorderRadius.circular(15)),
+              borderRadius: BorderRadius.circular(15)),
           width: double.infinity,
           child: Center(
             child: Text(
               widget.textName,
               style: TextStyle(
                   fontWeight: FontWeight.w900,
-                  fontSize: 19, color: Colors.white),
+                  fontSize: 19,
+                  color: Colors.white),
             ),
           ),
         ),
@@ -66,38 +65,39 @@ class MyTextField extends StatelessWidget {
   final TextEditingController textController;
   final IconData icon;
   final bool obscure;
+  final TextInputType keyboardType;
 
-  MyTextField({required this.name,required this.obscure,
-    required this.textController, required this.icon});
-
-
+  MyTextField(
+      {required this.name,
+      required this.obscure,
+      required this.textController,
+      required this.icon,
+      required this.keyboardType});
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: textController,
-      obscureText: obscure,
-      decoration: InputDecoration(
-        filled: true,
-        fillColor: Colors.white,
-        prefixIcon: Icon(
-          icon,
-          color: Colors.black,
+    return Container(
+      height: 60,
+      child: TextFormField(
+        controller: textController,
+        obscureText: obscure,
+        keyboardType: keyboardType,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          prefixIcon: Icon(
+            icon,
+            color: Colors.black,
+          ),
+          hintText: name,
+
+          hintStyle: TextStyle(fontSize: 19, color: Colors.black),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          errorBorder:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
+          floatingLabelBehavior: FloatingLabelBehavior.auto,
+          //labelText: 'Password',
         ),
-        hintText: name,
-        hintStyle: TextStyle(
-            fontSize: 19,
-            color: Colors.black
-        ),
-        border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10)
-        ),
-        errorBorder: OutlineInputBorder(
-            borderRadius:
-            BorderRadius.circular(5)),
-        floatingLabelBehavior:
-        FloatingLabelBehavior.auto,
-        //labelText: 'Password',
       ),
     );
   }
