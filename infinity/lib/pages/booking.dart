81ce365945afd8556ac30dc056652eb527b9c233
Darius <68332672+Darius253@users.noreply.github.com>
2021-11-20 02:29:11 +0000

    Added booking.dart

---
+++ infinity/lib/pages/booking.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Booking extends StatefulWidget {
+  const Booking({ Key? key }) : super(key: key);
+
+  @override
+  _BookingState createState() => _BookingState();
+}
+
+class _BookingState extends State<Booking> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
