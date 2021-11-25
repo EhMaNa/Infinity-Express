92db120287c42051cabbf20be830f67ac75dbb9a
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2021-11-25 11:19:54 +0000

    Written the code

--- infinity/lib/pages/timeline.dart
+++ infinity/lib/pages/timeline.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class Timeline extends StatefulWidget {
   @override
@@ -6,12 +7,62 @@ class Timeline extends StatefulWidget {
 }
 
 class _Timeline extends State<Timeline> {
+   List<String> imgList = [
+    'assets/images/infinity1.jpg',
+     'assets/images/infinity2.jpg',
+    'assets/images/infinity3.jpg',
+     'assets/images/infinity5.jpg',
+     'assets/images/infinity7.jpg',
+     'assets/images/infinity4.jpg',
+     'assets/images/infinity6.jpg'
+  ];
+
+   late List<Widget> imageSliders = imgList
+      .map((item) => Container(
+    child: Container(
+      margin: EdgeInsets.all(5.0),
+      child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+          child: Stack(
+            children: <Widget>[
+              Image.network(item, fit: BoxFit.cover, width: 1000.0),
+              Positioned(
+                bottom: 0.0,
+                left: 0.0,
+                right: 0.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromARGB(200, 0, 0, 0),
+                        Color.fromARGB(0, 0, 0, 0)
+                      ],
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                    ),
+                  ),
+                  padding: EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 20.0),
+                ),
+              ),
+            ],
+          )),
+    ),
+  ))
+      .toList();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          FractionallySizedBox(
+          CarouselSlider(
+            options: CarouselOptions(
+                height: 400.0,
+            autoPlay: true,
+            enlargeCenterPage: true),
+            items: imageSliders,
+          ),
+          /*FractionallySizedBox(
             alignment: Alignment.topCenter,
             heightFactor: 0.7,
             child: Container(
@@ -22,14 +73,14 @@ class _Timeline extends State<Timeline> {
                 ),
               ),
             ),
-          ),
-          FractionallySizedBox(
+          ),*/
+         /* FractionallySizedBox(
             alignment: Alignment.bottomCenter,
             heightFactor: 0.3,
             child: Container(
               color: Colors.white,
             ),
-          ),
+          ),*/
         ],
       ),
     );
