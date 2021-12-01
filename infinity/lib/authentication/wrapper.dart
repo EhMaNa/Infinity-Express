8b9f8c4136c18d8b2fbdc0425eaba625e68423fd
Darius <68332672+Darius253@users.noreply.github.com>
2021-12-01 22:50:16 +0000

    Added wrapper

---
+++ infinity/lib/authentication/wrapper.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+/*import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:infinity/pages/home.dart';
+import 'package:infinity/pages/welcome_page.dart';
+import 'package:provider/provider.dart';
+
+final usersRef = FirebaseFirestore.instance.collection('users');
+
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    if (user? == null) {
+      return WelcomePage();
+    }
+    return Home();
+  }
+}*/
