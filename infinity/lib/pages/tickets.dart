e7b06851b6b35eb63935fb12fea582cf19190fde
Darius <68332672+Darius253@users.noreply.github.com>
2021-12-01 22:55:21 +0000

    added code

--- infinity/lib/pages/tickets.dart
+++ infinity/lib/pages/tickets.dart
@@ -1,23 +1,66 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:infinity/authentication/auththentication.dart';
+import 'package:infinity/database/database.dart';
+import 'package:infinity/models/student.dart';
+import 'package:infinity/models/ticket.dart';
 
-class Tickets extends StatefulWidget {
-  const Tickets({ Key? key }) : super(key: key);
+class TicketWidget extends StatelessWidget {
+  const TicketWidget({Key? key}) : super(key: key);
 
-  @override
-  _TicketsState createState() => _TicketsState();
-}
-
-class _TicketsState extends State<Tickets> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'No tickets available',
-          style: TextStyle(color: Colors.red),
-        ),
-      ),
-      
-    );
+    Ticket ticket = Ticket();
+    Person person = Person();
+
+    final String currentUserID = Authentication().getCurrentUserID;
+
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirestoreService.ticketsCollection
+        .doc(currentUserID)
+          .collection('tickets')
+          .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return Center(child: Text(' No Tickets'));
+          List<QueryDocumentSnapshot> docSnaps = snapshot.data!.docs;
+          if (docSnaps.isEmpty)
+            return Center(
+                child: Text('No Tickets ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 21,
+                    )));
+          return ListView.builder(
+              itemCount: docSnaps.length,
+              itemBuilder: (context, index) {
+                 return Padding(
+                   padding: const EdgeInsets.all(8.0),
+                   child: Container(
+                    child: Card(
+                      child: ListTile(
+                        leading: Image.asset('assets/infinity1.jpg'),
+                        title: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Name: Darius'),
+                            Text('Destination: Sunyani'),
+                            Text('Departure: 27/11/21'),
+                            Text('PickUp: Athletic'),
+                            Text('TicketID:  5322145'),
+                            Text('SeatNumber: 20')
+                          ],
+                        ),
+                        trailing: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text('GHC 100'),
+                          ],
+                        ),
+                      ),
+                    ),
+                ),
+                 );
+              });
+        });
   }
 }
